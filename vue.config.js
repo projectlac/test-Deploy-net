59c8741a6116baaa03573ea9cdd1ba29d9c06657
projeclac <dft1711198@gmail.com>
2021-11-25 09:27:32 +0700

    huhu

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/ban-pick/" // Thay tên repository của các bạn vào đây nhé
+      ? "./" // Thay tên repository của các bạn vào đây nhé
       : "./",
   devServer: {
     port: 3000,
